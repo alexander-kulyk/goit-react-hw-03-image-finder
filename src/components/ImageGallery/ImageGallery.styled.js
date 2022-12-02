b616607b6dd46b678298e69df97397541709595f
alexander-kulyk <kulyk.alexander@gmail.com>
2022-12-02 17:45:13 +0200

    style search bar / gallery

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -2,7 +2,9 @@
 import styled from 'styled-components';
 
 const ImgList = styled.ul`
-  
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
 `;
 
 export default ImgList;
--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -2,7 +2,12 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100px;
+    background-color: aliceblue;
 `;
 
 export const Form = styled.form`
@@ -14,8 +19,22 @@ export const Input = styled.input`
 `;
 
 export const SearchBtn = styled.button`
-  
+   border: none;
+   margin-left: 10px ;
+
+    width: 80px;
+    height: 30px;
+
+    border-radius: 5px;
+
+    cursor: pointer;
+
+    :hover{
+        transform: scale(1.1)
+    }
 `;
 export const NameBtn = styled.span`
+    
+   
   
 `;
