714a5f53530ff4efa2567747d380456e457f457c
alexander-kulyk <kulyk.alexander@gmail.com>
2022-12-03 21:06:52 +0200

    Update Button.js

--- src/components/ButtonLoadMore/Button.js
+++ src/components/ButtonLoadMore/Button.js
@@ -1,9 +1,23 @@
-import { Button } from "./Button.styled";
+import PropTypes  from "prop-types";
+
+
+import { SearchBtn } from "components/Searchbar/Searchbar.styled";
+// import { Button } from "./Button.styled";
 
 
 
 export const ButtonLoadMore = ({onClickBtn, disabled}) =>{
     return(
-        <Button type="button" disabled={disabled} onClick={onClickBtn}>Load More</Button>
+        <div style={{display:'flex', alignItems:'center',justifyContent:'center'}}>
+            <SearchBtn type="button" disabled={disabled} onClick={onClickBtn}>Load More</SearchBtn>
+        </div>
+        
     )
+}
+//<Button type="button" disabled={disabled} onClick={onClickBtn}>Load More</Button>
+
+
+ButtonLoadMore.propTypes = {
+    onClickBtn: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired,
 }
